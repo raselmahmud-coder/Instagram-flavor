387ca8b5a88c452a8edff8e6a7f5b00e158c66f4
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-03-11 00:18:40 +0800

    user profile issue solved

--- js/app.js
+++ js/app.js
@@ -53,7 +53,8 @@ const switchTab = (id) => {
 const createPost = (post) => {
     const image = post.image;
     const div = document.createElement( "article" );
-    div.classList.add( "post" );
+  div.classList.add("post");
+  const userImage = `https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`;
     div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
@@ -62,7 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -107,7 +108,7 @@ const createPost = (post) => {
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
-                      <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
+                      <img src="${userImage}" alt="User Picture" />
                     </a>
 
                     <span>Liked by
